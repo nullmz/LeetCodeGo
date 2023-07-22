a71a93be650b2fd8bd12939230c3b1b71d1fdade
qhangz <qhangz@163.com>
2023-07-23 01:14:52 +0800

    快慢指针

---
+++ LeetCode/283.移动零.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=golang
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+func moveZeroes(nums []int) {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != 0 {
+			nums[fast], nums[slow] = nums[slow], nums[fast]
+			slow++
+		}
+	}
+}
+
+// @lc code=end
+
