c8a1db90c9b9676bc772cfeddab881c4a906c9ee
qhangz <qhangz@163.com>
2023-07-28 11:35:33 +0800

    移除链表元素

---
+++ LeetCode/203.移除链表元素.go
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=golang
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
+// 设置虚拟头节点进行删除操作
+func removeElements(head *ListNode, val int) *ListNode {
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+	cur := dummyHead
+	for cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummyHead.Next
+}
+
+// @lc code=end
+
