868ee31b8dfdd584e9f663554b81125fecb34758
qhangz <qhangz@163.com>
2023-08-03 01:15:32 +0800

    快乐数

---
+++ LeetCode/202.快乐数.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=202 lang=golang
+ *
+ * [202] 快乐数
+ */
+
+// @lc code=start
+func isHappy(n int) bool {
+	m := make(map[int]bool)
+	for n != 1 && !m[n] {
+		n, m[n] = getSum(n), true
+	}
+	return n == 1
+}
+
+func getSum(n int) int {
+	sum := 0
+	for n > 0 {
+		sum += (n % 10) * (n % 10)
+		n = n / 10
+	}
+	return sum
+}
+
+// @lc code=end
+
