c230af3a696138dad63bc5511055f9f57cf81316
qhangz <qhangz@163.com>
2023-08-06 00:55:44 +0800

    实现strStr()

---
+++ LeetCode/28.找出字符串中第一个匹配项的下标.go
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=golang
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+// @lc code=start
+// 方法一：前缀表减一实现
+// getNext 构造前缀表next
+// params:
+//		  next 前缀表数组
+//		  s 模式串
+func getNext(next []int, s string) {
+	j := -1 // j表示最长相等前后缀长度
+	next[0] = j
+
+	for i := 1; i < len(s); i++ {
+		for j >= 0 && s[i] != s[j+1] {
+			j = next[j] // 回退前一位
+		}
+		if s[i] == s[j+1] {
+			j++
+		}
+		next[i] = j // next[i]是i（包括i）之前的最长相等前后缀长度
+	}
+}
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	next := make([]int, len(needle))
+	getNext(next, needle)
+	j := -1 // 模式串的起始位置next为-1 因此j也为-1
+	for i := 0; i < len(haystack); i++ {
+		for j >= 0 && haystack[i] != needle[j+1] {
+			j = next[j] // 寻找下一个匹配点
+		}
+		if haystack[i] == needle[j+1] {
+			j++
+		}
+		if j == len(needle)-1 { // j指向了模式串的末尾
+			return i - len(needle) + 1
+		}
+	}
+	return -1
+}
+
+// @lc code=end
+
